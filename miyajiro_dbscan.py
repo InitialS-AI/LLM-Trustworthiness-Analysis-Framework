742f4d537ac2ee58f56b52a21ced4599346762d6
miyajiro <38776249+miyajiro@users.noreply.github.com>
2024-01-21 18:04:15 +0000

    w

--- miyajiro_dbscan.py
+++ miyajiro_dbscan.py
@@ -27,7 +27,7 @@ def main():
     abstraction_methods = ["DBSCAN"]
     abstract_state_nums = [200, 400, 600]
     epsilons = [0.000001, 0.00001, 0.0001, 0.001, 0.01, 0.1, 1]
-    pca_dims = [128, 256, 512]
+    pca_dims = [64, 128, 256, 512]
     probability_models = ["DTMC"]
     hmm_n_comps = [100, 200, 400]
     state_abstract_args = {
